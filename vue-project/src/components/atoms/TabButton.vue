bcbb360ae1ad591feea48d7bf3ce4d2b9e996886
Lerinskaya <rammnescence1@gmail.com>
2022-10-12 09:33:05 +0300

    s

---
+++ vue-project/src/components/atoms/TabButton.vue
@@ -0,0 +1,48 @@
+<template>
+  <button
+    :class="[$style.tab, { [$style.active]: isActive }]"
+    @click="$emit('click')"
+  >
+    {{ title }}
+  </button>
+</template>
+
+<script>
+export default {
+  props: {
+    title: String,
+    isActive: {
+      type: Boolean,
+      default: false,
+    },
+  },
+};
+</script>
+
+<style lang="scss" module>
+.tab {
+  font-family: "Open Sans";
+  background: #ffca93;
+  font-weight: 600;
+  font-size: 20px;
+  color: rgba(127, 75, 19, 0.42);
+  border: none;
+  cursor: pointer;
+  @media (max-width: 450px) {
+    font-size: 0.8rem;
+  }
+}
+
+button {
+  margin-right: 0;
+}
+.active {
+  border: 1px solid $brownBorder;
+  border-radius: 10px;
+}
+@media (max-width: 980px) {
+  p {
+    margin-right: 0.5rem;
+  }
+}
+</style>
---
+++ vue-project/src/components/organisms/TaskArea.vue
@@ -0,0 +1,86 @@
+<template>
+  <div :class="$style.taskArea">
+    <Task
+      v-for="todo in filterTasks"
+      :key="todo.id"
+      :id="todo.id"
+      :title="todo.title"
+      :isChecked="todo.isChecked"
+      @delete="deleteTodo(todo.id)"
+      @changes="changeTaskStatus(todo.id)"
+    />
+
+    <AddTask @keyup="addTodoItem" @change="todoTextChange" v-model="todoText" />
+  </div>
+</template>
+
+<script>
+import { mapGetters, mapMutations } from "vuex";
+import Task from "@/components/molecules/Task";
+import AddTask from "@/components/atoms/AddTask";
+export default {
+  data() {
+    return {
+      todoText: "",
+    };
+  },
+  methods: {
+    ...mapMutations(["changeTaskStatus"], ["deleteTask"], ["addTodo"]),
+    deleteTodo(id) {
+      this.$store.commit("deleteTask", id);
+    },
+    addTodoItem() {
+      this.$store.commit("addTodo", this.todoText);
+      this.todoText = "";
+    },
+  },
+  computed: {
+    ...mapGetters(["filterTasks"]),
+  },
+  components: {
+    Task,
+    AddTask,
+  },
+};
+</script>
+
+<style lang="scss" module>
+.taskArea {
+  background-color: $lightPeach;
+  padding-top: 1.875rem;
+  padding-bottom: 1.875rem;
+  @media (max-width: 800px) {
+    padding-top: 1rem;
+  }
+}
+.add {
+  font-family: "Inter";
+  padding: 0.688rem 0;
+  border-radius: 0.625rem;
+  display: flex;
+  color: $lightOrange;
+  background-color: $peach;
+  border: 2px dashed $lightOrange;
+  max-width: 30rem;
+  @media (max-width: 980px) {
+    max-width: 30rem;
+    margin: 1rem;
+  }
+  @media (max-width: 450px) {
+    margin-bottom: 0;
+  }
+  input {
+    font-weight: 400;
+    font-size: 1.25rem;
+    color: $lightOrange;
+    background-color: $peach;
+    border: none;
+    display: flex;
+    text-align: center;
+    @media (max-width: 450px) {
+      max-width: 10rem;
+      font-size: 1rem;
+    }
+  }
+}
+</style>
