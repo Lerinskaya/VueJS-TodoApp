70e21a4eaaf94c4df36bcde1992e10824c4fec8b
Lerinskaya <rammnescence1@gmail.com>
2022-11-08 16:11:00 +0300

    publicPath

--- vue-project/vue.config.js
+++ vue-project/vue.config.js
@@ -1,7 +1,7 @@
 const { defineConfig } = require("@vue/cli-service");
 
 module.exports = defineConfig({
-  publicPath: process.env.NODE_ENV === "production" ? "/ToDo-VUE-project/" : "/",
+  publicPath: process.env.NODE_ENV === "production" ? "/VueJS-TodoApp/" : "/",
   transpileDependencies: true,
   lintOnSave: false,
   css: {
